5d584a25fb146bbf4679e422a8b0dfaf3d81abc6
Chris Cone <chris.cone@ou.edu>
2016-08-25 14:04:41 -0500

    Fixed solr install issue

--- files/repo.enable.sh
+++ files/repo.enable.sh
@@ -4,3 +4,4 @@
 drush -r /srv/repository/drupal -y -u 1 en islandora
 drush -r /srv/repository/drupal -y -u 1 en islandora_basic_collection
 drush -r /srv/repository/drupal -y -u 1 en imagemagick islandora_basic_image
+drush -r /srv/repository/drupal -y -u 1 en islandora_solr
