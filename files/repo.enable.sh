7544894786ca708a4713110242034eacff9569cc
Logan Cox <lmc@ou.edu>
2016-08-30 13:47:13 -0500

    Properly define drush root for vset commands.

--- files/repo.enable.sh
+++ files/repo.enable.sh
@@ -6,21 +6,19 @@ ISLANDORA_ROOT="/srv/repository/drupal"
 
 drush -r "$ISLANDORA_ROOT" -y -u 1 en islandora islandora_basic_collection php_lib
 
+# Solr, forms, and metadata
 drush -r "$ISLANDORA_ROOT" -y -u 1 en objective_forms xml_forms xml_form_builder xml_schema_api xml_form_elements xml_form_api 
-
 drush -r "$ISLANDORA_ROOT" -y -u 1 en islandora_solr islandora_solr_config
 
 
 # Basic Image and Large Image
-
 drush -r "$ISLANDORA_ROOT" -y -u 1 en imagemagick islandora_basic_image islandora_large_image islandora_openseadragon
-drush eval "variable_set('islandora_book_page_viewers', array('name' => array('none' => 'none', 'islandora_openseadragon' => 'islandora_openseadragon'), 'default' => 'islandora_openseadragon'))"
+drush -r "$ISLANDORA_ROOT" eval "variable_set('islandora_book_page_viewers', array('name' => array('none' => 'none', 'islandora_openseadragon' => 'islandora_openseadragon'), 'default' => 'islandora_openseadragon'))"
 
 
 # Book
 drush -r "$ISLANDORA_ROOT" -y -u 1 en islandora_paged_content islandora_book
-drush eval "variable_set('islandora_large_image_viewers', array('name' => array('none' => 'none', 'islandora_openseadragon' => 'islandora_openseadragon'), 'default' => 'islandora_openseadragon'))"
-
+drush -r "$ISLANDORA_ROOT" eval "variable_set('islandora_large_image_viewers', array('name' => array('none' => 'none', 'islandora_openseadragon' => 'islandora_openseadragon'), 'default' => 'islandora_openseadragon'))"
 
 # Sample Content Generator
 drush -r "$ISLANDORA_ROOT" -y -u 1 en islandora_batch islandora_book_batch islandora_scg
