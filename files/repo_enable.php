0e8b38283d7d48f5dded88c71ea4a5459bd5ffbf
vagrant <vagrant@localhost.localdomain>
2016-09-22 15:12:32 -0500

    fix some bugs

--- files/repo_enable.php
+++ files/repo_enable.php
@@ -111,4 +111,4 @@ drupal_flush_all_caches();
 module_enable( array('ctools', 'link', 'themekey'));
 module_enable( array('oembed', 'oembed_provider', 'oembed_field'));
 module_enable( array('islandora_embed', 'ou_bagit_importer', 'islandora_internet_archive_bookreader_custom'));
-theme_enable( array('entity_iframe_theme', $islandora_embed_theme));
+theme_enable( array('entity_iframe_theme', 'islandora_embed_theme'));
